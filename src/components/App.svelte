c1ff1a1d0b3cb9c91f1ced9fd526b26f11b97ee8
Barry Postma <ict@barrypostma.com>
2021-10-27 22:14:49 +0100

    Introduce settings screen with basic routing & nav
    Running this build successfully requires a patch as mentioned in this PR: https://github.com/orlov-vo/parcel-transformer-svelte/pull/26.

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,24 @@
+<script>
+    import { Router, Link, Route } from "svelte-routing";
+    import Settings from "./Settings.svelte";
+    
+    export let name = "world";
+
+    const settings = () => {
+        console.log("settings!");
+    }
+</script>
+
 <h1>Hello {name}</h1>
 
-<script>
-    export let name = "world";
-</script>
+<Router>
+    <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="settings">Settings</Link>
+    </nav>
+    
+    <div id="pane">
+        <Route path="settings" component={Settings} />
+        <Route path="/">no content</Route>
+    </div>
+</Router>
---
+++ src/components/Settings.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { BrowserStorage } from "../clients/BrowserStorage";
+
+    const settings = BrowserStorage.getSettings();
+
+    const saveEventHandler = (e) => {
+        e.preventDefault();
+        BrowserStorage.saveSettings(settings);
+    }
+</script>
+
+<form class="pure-form pure-form-aligned">
+    <fieldset>
+        <legend>Loft AWS Settings</legend>
+        
+        <div class="pure-control-group">
+            <label for="instance">Loft instance name</label>
+            <input type="text" id="instance" placeholder="e.g. mydomain_com" bind:value={settings.instanceName} />
+        </div>
+        
+        <div class="pure-control-group">
+            <label for="region">AWS region</label>
+            <input type="text" id="region" placeholder="e.g. aq-central-9" bind:value={settings.region} />
+        </div>
+        
+        <div class="pure-control-group">
+            <label for="tableName">DynamoDB table name</label>
+            <input type="text" id="tableName" placeholder="e.g. loft-mydomain_com" bind:value={settings.tableName} />
+        </div>
+        
+        <div class="pure-control-group">
+            <label for="queuePrefix">SQS queue prefix</label>
+            <input type="text" id="queuePrefix" placeholder="e.g. loft-mydomain_com" bind:value={settings.queuePrefix} />
+        </div>
+        
+        <div class="pure-control-group">
+            <label for="identityPoolId">Cognito identity pool id</label>
+            <input type="text" id="identityPoolId" placeholder="e.g. aq-central-9:a0b1c2..." bind:value={settings.identityPoolId} />
+        </div>
+        
+        <div class="pure-control-group">
+            <label for="userPoolId">Cognito user pool id</label>
+            <input type="text" id="userPoolId" placeholder="e.g. aq-central-9_0a1b2c..." bind:value={settings.userPoolId} />
+        </div>
+        
+        <div class="pure-control-group">
+            <label for="userPoolWebClientId">Cognito user pool web client id</label>
+            <input type="text" id="userPoolWebClientId" placeholder="e.g. 0foo1bar2baz345..." bind:value={settings.userPoolWebClientId} />
+        </div>
+        
+        <div class="pure-controls">
+            <button type="submit" class="pure-button pure-button-primary" on:click={saveEventHandler}>Save</button>
+        </div>
+    </fieldset>
+</form>
